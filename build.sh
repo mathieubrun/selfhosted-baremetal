bd82924e732b0cdb18f928c5f5670e4990e22653
Mathieu BRUN <>
2022-07-14 22:36:24 +0200

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-cat flatcar.config.yml | docker run --rm -i ghcr.io/flatcar-linux/ct:latest > ignition.json
+docker run -i --rm quay.io/coreos/butane:release --pretty --strict < flatcar.config.yml > ignition.json
 packer build -force .
+cd builds
+vagrant up
+vagrant ssh
+vagrant destroy --force
+cd -
